8c25be3604bce453afb3b7327e30c26b33a2675d
John Haste <joaovictorspteixeira@gmail.com>
2022-04-29 15:52:49 -0300

    Player Updates Y Rotation When Moving

--- top-down-adventure/Assets/PlayerController.cs
+++ top-down-adventure/Assets/PlayerController.cs
@@ -25,8 +25,17 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        //Get the direction of Z and X
-        direction = new Vector3(horizontal, 0f, vertical);
+        //Get the direction of Z and X (Normalize so diagonal won't be faster)
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        //If it's moving
+        if(direction.magnitude > 0.1){
+            //Gets Tangent of the 2 positions x and z (0 to 1f) and Convert Rad to Degrees
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            //Updates Y rotation
+            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+        }
+
         //Makes the character move using the CharacterController Componente
         //Multiplies by speed and deltatime(So FPS won't affect speed)
         myCharacterController.Move(direction * movementSpeed * Time.deltaTime);
