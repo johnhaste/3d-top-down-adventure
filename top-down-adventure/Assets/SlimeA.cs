ffa61fb5fe46a47547f50d362f54fb7ea6a126a9
John Haste <joaovictorspteixeira@gmail.com>
2022-04-30 17:37:19 -0300

    Enemy Dies, Flashes and Disappears

--- top-down-adventure/Assets/SlimeA.cs
+++ top-down-adventure/Assets/SlimeA.cs
@@ -7,11 +7,16 @@ public class SlimeA : MonoBehaviour
 
     private Animator myAnimator;
     public ParticleSystem fxHit;
+    public int HP = 3;
+    private bool isDead;
+    public GameObject slimeBody;
+    private SkinnedMeshRenderer skinnedMeshRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         myAnimator = GetComponent<Animator>();
+        skinnedMeshRenderer = slimeBody.GetComponent<SkinnedMeshRenderer>();
     }
 
     // Update is called once per frame
@@ -23,10 +28,37 @@ public class SlimeA : MonoBehaviour
     #region MY_METHODS
 
     void GetHit(int amount){
+
+        //If it's dead, does'nt get hit
+        if(isDead){return;}
         
-        myAnimator.SetTrigger("GetHit");
-        fxHit.Emit(10);
+        if(HP > 1){
+            myAnimator.SetTrigger("GetHit");
+            fxHit.Emit(10);
+            HP--;
+        }else{
+            myAnimator.SetTrigger("Die");
+            StartCoroutine("Died");
+        }
         
+    }
+
+    IEnumerator Died(){
+        isDead = true;
+        yield return new WaitForSeconds(0.2f);
+        skinnedMeshRenderer.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        skinnedMeshRenderer.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        skinnedMeshRenderer.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        skinnedMeshRenderer.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        skinnedMeshRenderer.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        skinnedMeshRenderer.enabled = true;
+        yield return new WaitForSeconds(0.5f);
+        Destroy(this.gameObject);
     }
 
     #endregion
