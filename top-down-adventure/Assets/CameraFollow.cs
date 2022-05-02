fd7bb582f80ced4e2da41b45363b8d08d7b6be8d
John Haste <joaovictorspteixeira@gmail.com>
2022-05-01 21:10:18 -0300

    Fixed Camera with Camera Follow Object

---
+++ top-down-adventure/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position = new Vector3(player.transform.position.x,player.transform.position.y,player.transform.position.z);        
+    }
+}
